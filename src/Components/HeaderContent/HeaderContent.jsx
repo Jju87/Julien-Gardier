d7261fb70c7c69480475d7cbeae7d7f7a381a179
Julien <gardier.julien@gmail.com>
2024-05-10 12:03:47 +0200

    front-end initial commit

---
+++ src/Components/HeaderContent/HeaderContent.jsx
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from 'react';
+import portrait from '../../Assets/Images/portrait.jpg'
+import portrait2 from '../../Assets/Images/Portrait-fond-transparent.png'
+
+import './headercontent.scss';
+
+function HeaderContent() {
+    const [indexString, setIndexString] = useState(0);
+    const [indexArray, setIndexArray] = useState(0);
+    const [textDisplay, setTextDisplay] = useState('');
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [isAnimationStarted, setIsAnimationStarted] = useState(false); // Nouvel état pour suivre le démarrage de l'animation
+
+    const text = ["Développeur Web MERN      ", "MongoDB", "Express", "React", "Node.js"]
+
+    const randomSpeed = (min, max) => { 
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsAnimationStarted(true); // Mettre à jour l'état pour indiquer que l'animation peut commencer
+        }, 1000); // Délai de 2 secondes avant de démarrer l'animation
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    useEffect(() => {
+        if (!isAnimationStarted) return; // Ne rien faire tant que l'animation n'a pas démarré
+
+        const animationTimer = setInterval(() => {
+            if (isDeleting) {
+                setTextDisplay(prev => prev.slice(0, prev.length - 1));
+                if (textDisplay === '') {
+                    setIsDeleting(false);
+                    setIndexArray(prev => (prev + 1) % text.length);
+                    randomSpeed(20, 50);
+                }
+            } else {
+                setTextDisplay(text[indexArray].slice(0, indexString));
+                setIndexString(prev => prev + 1);
+                if (indexString > text[indexArray].length) {
+                    setIndexString(0);
+                    setIsDeleting(true);
+                }
+            }
+        }, isDeleting ? randomSpeed(20, 70) : randomSpeed(50, 270));
+
+        return () => clearInterval(animationTimer);
+    }, [indexString, indexArray, text, textDisplay, isDeleting, isAnimationStarted]);
+
+  return (
+    <section className="header-content">
+      <div className="introduction">
+        <span className="introduction__welcome">Bonjour, je suis</span>
+        <h1 className="introduction__name">Julien <span>Gardier</span></h1>
+            <span 
+                className={`introduction__skills${textDisplay ? "" : "--transparent"}`}>
+                {textDisplay ? textDisplay : "texte transparent"}
+            </span>
+      </div>
+      <div className="portrait-container">
+        <div className="portrait-container__frame">
+            <img src={portrait2} className="portrait-container__frame--portrait2" alt="portrait de Julien Gardier, développeur web"/>
+        </div>
+        <div className="portrait-container__strengths">
+        </div>
+      </div>
+    </section>
+  );
+}
+
+
+export default HeaderContent;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Import de la fonctionnalité de routage
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Home from './Pages/Home/Home';
+import Header from './Layout/Header/Header';
+
+import '../src/Styles/main.scss';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<React.StrictMode>
+        <Router>
+            <Header />
+            <main>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </main>
+        </Router>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
