611fc9f93156d4ee4f8356a68decdd682683d187
Julien <gardier.julien@gmail.com>
2024-05-16 15:34:22 +0200

    portfolio first slide and design implemented

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,22 @@
+import './card.scss';
+
+function Card() {
+    
+    
+    return (
+            <div className="card-groups">
+                <div className="card-group">
+                    <div className="little-card card"></div>
+                    <div className="big-card card"></div>
+                    <div className="little-card card"></div>
+                    <div className="big-card card"></div>
+                    <div className="little-card card"></div>
+                    <div className="big-card card"></div>
+                    <div className="little-card card"></div>
+                    <div className="big-card card"></div>
+                </div>
+            </div>
+    );
+}
+
+export default Card;
--- src/Layout/Francais/Portfolio/Portfolio.jsx
+++ src/Layout/Francais/Portfolio/Portfolio.jsx
@@ -1,18 +1,29 @@
 import './portfolio.scss';
+import Card from '../../../Components/Card/Card';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleChevronLeft, faCircleChevronRight } from '@fortawesome/free-solid-svg-icons';
+
 
 function Portfolio() {
   return (
     <section id="portfolio" className="portfolio">
         <div className="portfolio__container">
-        </div>
-        <div className="portfolio__content">
+            <div className="portfolio__content">
                 <h2>Portfolio</ h2>
-                <div className="portfolio__content--filter">
+                <div className="portfolio__content--filters">
                     <button>Tout</button>
                     <button>Frontend</button>
                     <button>Backend</button>
                     <button>Optimisation</button>
                 </ div>
+                <div className="portfolio__content--cards">
+                    < Card />
+                </div>
+                <div className="portfolio__content--arrows" >
+                    <FontAwesomeIcon icon={faCircleChevronLeft} />
+                    <FontAwesomeIcon icon={faCircleChevronRight} />
+                </div>
+            </div>
         </div>
     </section>
   );
