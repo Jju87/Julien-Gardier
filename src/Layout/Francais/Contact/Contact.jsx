8f31fef52041986009ad29333b92a26201fc40b0
Julien <gardier.julien@gmail.com>
2024-05-22 14:46:34 +0200

    contact section styled

--- src/Layout/Francais/Contact/Contact.jsx
+++ src/Layout/Francais/Contact/Contact.jsx
@@ -1,7 +1,10 @@
 import './contact.scss';
+import ReCAPTCHA from "react-google-recaptcha";
+import callMeImage from "../../../Assets/Images/call.jpg";
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faComments } from '@fortawesome/free-solid-svg-icons';
-import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faComments, faPhoneVolume  } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faGithub, faCodepen,} from '@fortawesome/free-brands-svg-icons';
 
 function Contact () {
     return (
@@ -20,24 +23,35 @@ function Contact () {
                                 <label htmlFor="message"></label>
                                 <textarea name="message" id="message" required placeholder="Votre message"></textarea>
                             </div>
+                            <div className="captcha">
+                                <ReCAPTCHA sitekey={process.env.REACT_APP_CAPTCHA_KEY} action='homepage' />
+                            </div>                           
                             <button type="submit">Envoyer</button>
                         </form>
                     </div>
                     <div className="contact__content--links">
-                        <div className="contact__content--links-socials">
+                        <FontAwesomeIcon className="phone-volume" icon={faPhoneVolume} />
+                        <div className="contact__content--links-image">
+                            <img src={callMeImage} alt="Call me, Julien Gardier" />
+                            <div className="contact__content--links-phone"></div>
+                            <button className="phone-btn">
+                                <a href="https://calendly.com/gardierjulien/15min" target="_blank" rel="noopener noreferrer">
+                                    <span>Planifiez un appel <FontAwesomeIcon icon={faComments} className="contact__content--links-phone-icon" /></span>
+                                </a>
+                            </button>
+                            
+                        </div>
+                        {/* <div className="contact__content--links-socials">
                             <a href="www.linkedin.com/in/julien-gardier" target="_blank" rel="noopener noreferrer">
                                 <FontAwesomeIcon icon={faLinkedin} className="contact__content--socials-icon" />
                             </a>
                             <a href="https://github.com/Jju87" target="_blank" rel="noopener noreferrer">
                                 <FontAwesomeIcon icon={faGithub} className="contact__content--socials-icon" />
                             </a>
-                        </div>
-                        <div className="contact__content--links-phone">
-                            <FontAwesomeIcon icon={faComments} className="contact__content--links-phone-icon" />
-                                <a href="https://calendly.com/gardierjulien/15min" target="_blank" rel="noopener noreferrer">
-                                    <button>Planifiez un appel</button>
-                                </a>
-                        </div>
+                            <a href="https://codepen.io/Gr1chk4" target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={faCodepen} className="contact__content--socials-icon" />
+                            </a>
+                        </div> */}
                     </div>
                 </div>               
             </div>
